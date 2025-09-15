1ae6feea709ab2b39322034963b729f6aee50b23
towromanterry-prog <towromanterry@gmail.com>
2025-09-15 12:13:31 +0300

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
